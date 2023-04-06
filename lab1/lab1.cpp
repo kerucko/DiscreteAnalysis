d1cff41b8d45ae1ee40553e314e39b1ad2990239
kerucko <cykinpavel@gmail.com>
2023-04-06 16:05:14 +0300

    lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string.h>
+
+struct Data {
+    int key;
+    char *string;
+};
+
+Data *InputData(int *size) {
+    Data *array = (Data *)malloc(1 * sizeof(Data));
+    int input_key;
+    char input_string[2048];
+    int capacity = 1;
+    while (scanf("%d%s", &input_key, input_string) != EOF) {
+        if (*size == capacity) {
+            array = (Data  *)realloc(array, 2 * capacity * sizeof(Data));
+            capacity *= 2;
+        }
+        array[*size].key = input_key;
+        array[*size].string = (char *)malloc((strlen(input_string) + 1) * sizeof(char));
+        for (int i = 0; i <= strlen(input_string); ++i) {
+            array[*size].string[i] = input_string[i];
+        }
+        ++(*size);
+    }   
+    return array;
+}
+
+void OutputData(Data *array, int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d\t%s\n", array[i].key, array[i].string);
+    }
+}
+
+Data *Sort(Data *array, int size) {
+    const int numbers = 65536;
+    int counter[numbers] = {0};
+    for (int i = 0; i < size; ++i) {
+        ++counter[array[i].key];
+    }
+    for (int i = 1; i < numbers; ++i) {
+        counter[i] += counter[i - 1];
+    }
+    Data *sorted_array = (Data *)malloc(size * sizeof(Data));
+    for (int i = size - 1; i >= 0; --i) {
+        sorted_array[counter[array[i].key] - 1] = array[i];
+        --counter[array[i].key];
+    }
+    return sorted_array;
+}
+
+int main() {
+    int size = 0;
+    Data *array = InputData(&size);
+    Data *result = Sort(array, size);
+    OutputData(result, size);
+    for (int i = 0; i < size; ++i) {
+        free(array[i].string);
+    }
+    free(array);
+    free(result);
+    return 0;
+}
---
+++ lab1/lab1_bubble.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string.h>
+
+typedef struct data {
+    int key;
+    char *string;
+} data;
+
+data *input_data(int *size) {
+    data *array = (data *)malloc(1 * sizeof(data));
+    int input_key;
+    char input_string[2048];
+    int capacity = 1;
+    while (scanf("%d%s", &input_key, input_string) != EOF) {
+        if (*size == capacity) {
+            array = (data  *)realloc(array, 2 * capacity * sizeof(data));
+            capacity *= 2;
+        }
+        array[*size].key = input_key;
+        array[*size].string = (char *)malloc((strlen(input_string) + 1) * sizeof(char));
+        for (int i = 0; i <= strlen(input_string); ++i) {
+            array[*size].string[i] = input_string[i];
+        }
+        ++(*size);
+    }   
+    return array;
+}
+
+void output_data(data *array, int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d\t%s\n", array[i].key, array[i].string);
+    }
+}
+
+void sort(data *array, int size) {
+    for (int i = 0; i < size; ++i){
+        for (int j = i + 1; j < size; ++j) {
+            if (array[i].key > array[j].key) {
+                data temperal = array[i];
+                array[i] = array[j];
+                array[j] = temperal;
+            }
+        }
+    }
+}
+
+int main() {
+    int size = 0;
+    data *array = input_data(&size);
+    sort(array, size);
+    output_data(array, size);
+    for (int i = 0; i < size; ++i) {
+        free(array[i].string);
+    }
+    free(array);
+    return 0;
+}
